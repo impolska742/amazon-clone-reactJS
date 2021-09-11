29c9102705bafa9ebb2ea410825ab5fc55cd07e7
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-09-11 16:30:59 +0530

    Home initialized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
-      {/* Header */}
-      {/* Home */}
+      <Home />
     </>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__img"
+          src="https://m.media-amazon.com/images/I/71R3l1JT1NL._SX3000_.jpg"
+          alt="Home Banner"
+        />
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
