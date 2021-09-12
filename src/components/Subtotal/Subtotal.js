3dd39e65041c193a64219cea30b8ee45c1b62b15
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-09-12 18:41:05 +0530

    Subtotal section added

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -1,8 +1,31 @@
 import React from "react";
+import CurrencyFormat from "react-currency-format";
 import "./Subtotal.css";
 
 const Subtotal = () => {
-  return <div className="subtotal">Subtotal here</div>;
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={() => (
+          <>
+            <p>
+              Subtotal (0 items) : <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift.
+            </small>
+          </>
+        )}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"₹"}
+        decimalScale={2}
+      />
+
+      <button className="subtotal__btn">Proceed to checkout</button>
+    </div>
+  );
 };
 
 export default Subtotal;
